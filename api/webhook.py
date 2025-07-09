9d0a29246d6a6542939bef916bef90b121c4385b
muou-hub <muou2021@gmail.com>
2025-07-10 03:18:56 +0800

    Update webhook.py

--- api/webhook.py
+++ api/webhook.py
@@ -1,6 +1,2 @@
-import requests
-
-DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/1392570606783959150/PKn8bECItnaFWk3TD_pWtse0Gn3PB17zVp_CzZoNSDaCsbU_2QmMyBeiAuEP1Hj5hJ0C"
-
 def handler(request):
     return ("Hello Vercel!", 200)
