4f353884dc021ec0a4853eb53f020bf96364ebcf
muou-hub <muou2021@gmail.com>
2025-07-10 03:10:46 +0800

    Update webhook.py

--- api/webhook.py
+++ api/webhook.py
@@ -1,6 +1,6 @@
 import requests
 
-DISCORD_WEBHOOK_URL = "你的 Discord Webhook URL"
+DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/1392570606783959150/PKn8bECItnaFWk3TD_pWtse0Gn3PB17zVp_CzZoNSDaCsbU_2QmMyBeiAuEP1Hj5hJ0C"
 
 def handler(request):
     if request.method == "POST":
